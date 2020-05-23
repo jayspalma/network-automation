c2f11317bd95b90b6011784172dfdf517e3546df
jayspalma <jayshobbyrc@gmail.com>
2020-05-23 18:45:01 +0800

    added functionality of connecting to devices using csv file

--- script/configure.py
+++ script/configure.py
@@ -1,4 +1,5 @@
 from napalm import get_network_driver
+import csv
 
 driver = get_network_driver("ios")
 device = driver("192.168.1.1", "jays", "jayspalma")
@@ -11,3 +12,19 @@ device = driver("192.168.1.2", "jays", "jayspalma")
 device.open()
 facts = device.get_facts()
 print(facts["hostname"])
+
+
+f = open("devices.csv")
+csv_f = csv.reader(f)
+
+for row in csv_f:
+    ios_family = row[0]
+    ip = row[1]
+    username = row[2]
+    password = row[3]
+
+    driver = get_network_driver(ios_family)
+    device = driver(ip, username, password)
+    device.open()
+    facts = device.get_facts()
+    print(facts["hostname"])
